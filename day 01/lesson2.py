54d4a03635d1868ff37c3c88ad16d2adb80b0685
datamachara <myname123myname321@gmail.com>
2024-10-19 15:31:37 +0400

    commit

---
+++ day 01/lesson2.py
@@ -0,0 +1,27 @@
+#name  = "data"
+
+
+surname  =  "machara"
+
+#print(name)
+
+
+lastname = " years old and i am "
+wight = "i am"
+name = "data"
+age = (13)
+height = 163.5 
+day = "cm old. i also play basketball, bye."
+
+
+knows_programming = True    
+is_ugly = False
+
+isUGly = False
+
+
+print(name + ' ' + surname)
+
+print (type(age)) 
+
+print(wight  + " " +  name + " " +  str(age) + " " + lastname  + " " + str(height) + " " + day  )
